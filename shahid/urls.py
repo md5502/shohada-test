0031e11f57e42ee7dd7cf931556898af3d3c7c76
md5502 <m.baniasadi.d@gmail.com>
2024-09-27 20:37:53 +0330

    add views for the qrcode and download images

--- shahid/urls.py
+++ shahid/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import shahid_detail, shahid_list
+from .views import download_shahid_images, generate_qr_code, shahid_detail, shahid_list
 
 app_name = "shahid"
 
 urlpatterns = [
     path("", shahid_list, name="shahid_list"),
     path("<str:slug>/", shahid_detail, name="shahid_detail"),
+    path("<str:slug>/qr/", generate_qr_code, name="generate_qr_code"),
+    path("shahid/<str:slug>/images/download/", download_shahid_images, name="download_shahid_images"),
 ]
--- shahid/views.py
+++ shahid/views.py
@@ -1,3 +1,8 @@
+import os
+import zipfile
+
+import qrcode
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Shahid
@@ -12,3 +17,36 @@ def shahid_list(request):
 def shahid_detail(request, slug):
     shahid = get_object_or_404(Shahid, slug=slug)
     return render(request, "shahid/detail.html", {"shahid": shahid})
+
+
+
+
+def generate_qr_code(request, slug):
+    shahid = get_object_or_404(Shahid, slug=slug)
+    qr_data = request.build_absolute_uri(f"/shahid/{shahid.slug}/")
+    qr = qrcode.make(qr_data)
+
+    response = HttpResponse(content_type="image/png")
+    qr.save(response, "PNG")
+    return response
+
+
+
+
+
+def download_shahid_images(request, slug):
+    shahid = get_object_or_404(Shahid, slug=slug)
+
+    # Set the zip file name to Shahid's name
+    zip_filename = f"{shahid.name}_{shahid.lname}_images.zip"  # Change this line
+
+    # Create a ZIP file in memory
+    response = HttpResponse(content_type="application/zip")
+    response["Content-Disposition"] = f"attachment; filename={zip_filename}"
+
+    with zipfile.ZipFile(response, "w") as zip_file:
+        for image in shahid.images.all():
+            image_path = image.image.path  # Get the image path
+            zip_file.write(image_path, os.path.basename(image_path))  # Write the image to the ZIP file
+
+    return response
