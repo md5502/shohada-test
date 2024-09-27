86ff73b922677875b1ac3f1e093f1158a80e65a4
md5502 <m.baniasadi.d@gmail.com>
2024-09-27 20:34:52 +0330

    fix the model

--- shahid/admin.py
+++ shahid/admin.py
@@ -2,23 +2,43 @@ import uuid
 
 from django.contrib import admin
 from django.utils.text import slugify
+from jalali_date.admin import ModelAdminJalaliMixin
 
-from .models import Shahid
+from .models import Image, Shahid
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1  # Allows adding an additional image in the Shahid form
 
 
 class ShahidAdmin(admin.ModelAdmin):
-    list_display = ("name", "lname", "martyrdom_location", "martyrdom_date", "slug", "qr_code_image")
-
-    # Do not display 'slug' in the form, it will be generated automatically
-    exclude = ("slug",)
-
-    readonly_fields = ("qr_code_image",)
+    list_display = (
+        "first_name",
+        "last_name",
+        "martyrdom_date",
+        "martyrdom_location",
+        "birth_date",
+        "birth_location",
+    )
+    # Remove slug from exclude to allow saving
+    search_fields = ("first_name", "last_name", "martyrdom_location")
+    inlines = [ImageInline]  # Adds inline images related to each Shahid
 
     def save_model(self, request, obj, form, change):
+        # Generate slug only if it's not provided
         if not obj.slug:
-            obj.slug = slugify(f" {str(uuid.uuid4()).split("-")[0]} {obj.name} {obj.lname} ",allow_unicode=True)
+            obj.slug = slugify(
+                f"{str(uuid.uuid4()).split('-')[0]} {obj.first_name} {obj.last_name}",
+                allow_unicode=True,
+            )
         super().save_model(request, obj, form, change)
 
 
-# Register the Shahid model and the ShahidAdmin class
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("owner", "image")
+    search_fields = ("owner__first_name", "owner__last_name")  # Allows searching by Shahid's first and last name
+
+
 admin.site.register(Shahid, ShahidAdmin)
+admin.site.register(Image, ImageAdmin)
--- shahid/models.py
+++ shahid/models.py
@@ -1,15 +1,38 @@
 from django.db import models
-
+from django_jalali.db import models as jmodels
 
 class Shahid(models.Model):
-    name = models.CharField(max_length=200)
-    lname = models.CharField(max_length=200, verbose_name="last name")
-    martyrdom_location = models.CharField(max_length=200)
-    martyrdom_date = models.DateField()
-    description = models.TextField()
-    image = models.ImageField(upload_to="shohada_images")
-    slug = models.SlugField(null=True, blank=True, unique=True, allow_unicode=True)
-    qr_code_image = models.ImageField(upload_to="shohada_qrcodes", null=True, blank=True)
+    first_name = models.CharField(max_length=200, verbose_name="نام")
+    last_name = models.CharField(max_length=200, verbose_name="نام خانوادگی")
+    martyrdom_location = models.CharField(max_length=200, verbose_name="محل شهادت")
+    martyrdom_date = jmodels.jDateField(verbose_name="تاریخ شهادت")
+    birth_date = jmodels.jDateField(null=True, blank=True, verbose_name="تاریخ تولد")
+    birth_location = models.CharField(
+        max_length=200, null=True, blank=True, verbose_name="محل تولد",
+    )  # مکان تولد
+    description = models.TextField(verbose_name="توضیحات")
+    will = models.TextField(null=True, blank=True, verbose_name="وصیت‌نامه")
+    important_quotes = models.TextField(
+        null=True, blank=True, verbose_name="نقل‌قول‌های مهم",
+    )
+    slug = models.SlugField(
+        null=True, blank=True, unique=True, allow_unicode=True, verbose_name="شناسه",
+    )
 
     def __str__(self) -> str:
-        return f"{self.name} {self.lname}"
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:  # noqa: DJ012
+        verbose_name = "شهید"
+        verbose_name_plural = "شهدا"
+
+
+class Image(models.Model):  # noqa: DJ008
+    image = models.ImageField(upload_to="shohada_images", verbose_name="عکس")
+    owner = models.ForeignKey(
+        Shahid, on_delete=models.CASCADE, related_name="images", verbose_name="شهید",
+    )
+
+    class Meta:
+        verbose_name = "عکس"
+        verbose_name_plural = "عکس‌ها"  # noqa: RUF001
